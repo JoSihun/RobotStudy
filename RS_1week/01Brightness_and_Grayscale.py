71739969aa786e3c367c644a52c9efea15b035eb
VncntKo <dudwls3703@naver.com>
2021-02-04 19:33:13 +0900

    2021.02.04(목) / 19:33

--- RS_1week/01Brightness_and_Grayscale.py
+++ RS_1week/01Brightness_and_Grayscale.py
@@ -9,9 +9,9 @@ FullName = Path + Name
 img = cv.imread(FullName, 1)   # BGR 컬러로 읽기
 gray1 = cv.imread(FullName, 0)  # 흑백으로 읽기
 
-print("그레이 이미지", gray1.shape)
-print("컬러 이미지 ", img.shape)
-print("----------------------------------------")
+# print("그레이 이미지", gray1.shape)
+# print("컬러 이미지 ", img.shape)
+# print("----------------------------------------")
 
 
 # ==================== 밝기 조절1 (픽셀 단위: for문 이용) ====================
@@ -20,19 +20,23 @@ print("----------------------------------------")
 # 3채널 컬러 이미지는 픽셀값이 각 채널의 색상 값을 나타냄
 # modulo 연산 / numpy 연산
 
+# #******** 1) 그레이 이미지에 밝기 +50한 것을 bright에 넣기 ********#
 # bright = np.zeros(gray1.shape)      # gray 이미지 틀에 0으로 초기화된 2중 리스트
 # for y in range(gray1.shape[0]):
 #     for x in range(gray1.shape[1]):
-#         #******** 1) 그레이 이미지에 밝기 +50한 것을 bright에 넣기 ********#
-
+#         bright[y][x] = gray1[y][x] + 50
+#         if bright[y][x] > 255: bright[y][x] = 255
 #
-#
+# #******** 2) 그레이 이미지에 밝기 -50한 것을 dark에 넣기 ********#
 # dark = np.zeros(gray1.shape)
 # for y in range(gray1.shape[0]):
 #     for x in range(gray1.shape[1]):
-#         #******** 2) 그레이 이미지에 밝기 -50한 것을 dark에 넣기 ********#
-
+#         dark[y][x] = gray1[y][x] - 50
+#         if dark[y][x] < 0: dark[y][x] = 0
 #
+# cv.imshow('gray', gray1)
+# cv.imshow('bright', bright.astype('uint8'))
+# cv.imshow('dark', dark.astype('uint8'))
 #
 # print("*************** gray ***************\n원소 데이터 타입:", type(gray1[0][0]), "\n", gray1)
 # print("\n************** bright **************\n원소 데이터 타입:", type(bright[0][0]), "\n", bright)
@@ -45,11 +49,9 @@ print("----------------------------------------")
 # bright = cv.add(gray1, 100)
 # dark = cv.add(gray1, -100)
 #
-#
 # cv.imshow('gray', gray1)
 # cv.imshow('bright', bright)
 # cv.imshow('dark', dark)
-
 
 
 # ========================= Gray Scale =========================
@@ -58,14 +60,16 @@ print("----------------------------------------")
 # 2. gray = R * 0.2126 + G * 0.7152 + B * 0.0722
 
 # gray2 = # ************* 3) 1번 공식 사용하여 회색조 이미지 만들기 ***********
-# gray3 = img[:, :, 2] * 0.2126 + img[:, :, 1] * 0.7152 + img[:, :, 0] * 0.0722
-#
-# cv.imshow("color", img)
-# cv.imshow("gray2", gray2 / 255)
-# cv.imshow("gray3", gray3 / 255)
-#
-# # #OpenCV 함수 사용
-# gray4 = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-# cv.imshow("gray4", gray4)
+B, G, R = cv.split(img)
+gray2 = R / 3 + B / 3 + G / 3
+gray3 = img[:, :, 2] * 0.2126 + img[:, :, 1] * 0.7152 + img[:, :, 0] * 0.0722
+
+cv.imshow("color", img)
+cv.imshow("gray2", gray2 / 255)
+cv.imshow("gray3", gray3 / 255)
+
+# #OpenCV 함수 사용
+gray4 = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow("gray4", gray4)
 
 cv.waitKey()
--- RS_1week/02ROI_Resize_Concatenate.py
+++ RS_1week/02ROI_Resize_Concatenate.py
@@ -17,16 +17,16 @@ print("----------------------------------------")
 
 # ==================== ROI 설정 ====================
 
-ROI = np.zeros(img.shape, np.uint8)      # gray 이미지 틀에 0으로 초기화된 2중 리스트
-for y in range(img.shape[0]):
-    for x in range(img.shape[1]):
-        if # ******* 1) y 가 300 미만일 때 ***********:
-            ROI[y][x] = img[y][x]
-        else:
-            ROI[y][x] = 0
-
-cv.imshow("origin", img)
-cv.imshow("ROI", ROI)
+# ROI = np.zeros(img.shape, np.uint8)     # gray 이미지 틀에 0으로 초기화된 2중 리스트
+# for y in range(img.shape[0]):
+#     for x in range(img.shape[1]):
+#         if y < 300:                     # ******* 1) y 가 300 미만일 때 ***********:
+#             ROI[y][x] = img[y][x]
+#         else:
+#             ROI[y][x] = 0
+#
+# cv.imshow("origin", img)
+# cv.imshow("ROI", ROI)
 
 
 # ==================== resize =========================
@@ -36,27 +36,32 @@ cv.imshow("ROI", ROI)
 #  *fx:             가로 사이즈 배수   ex) fx=2
 #  *fy:             세로 사이즈 배수   ex) fy=4
 #  *interpolation:  보간법
-
-# resize1 = cv.resize(img, (150, 150))
-# #resize2 = ******** 2) 가로 0.5배, 세로 0.6배로 리사이즈 하기 hint: dsize (0, 0)으로 초기화 해야함)************
 #
+# resize1 = cv.resize(img, (150, 150))
+# resize2 = cv.resize(img, (0, 0), fx=0.5, fy=0.6)
+# #resize2 = cv.resize(img, (int(img.shape[1] * 0.5), int(img.shape[0] * 0.6)))
+# #resize2 = ******** 2) 가로 0.5배, 세로 0.6배로 리사이즈 하기 hint: dsize (0, 0)으로 초기화 해야함)************
 #
 # cv.imshow("origin", img)
 # cv.imshow("resize1", resize1)
-#cv.imshow("resize2", resize2)
+# cv.imshow("resize2", resize2)
 
 
 # ================== 이미지 합치기 =========================
 # img는 512x512
 # img_4color는 100x100
-#
+# ******* 3) 두 이미지의 좌측 상단 모서리가 닿도록 이미지 합치기 ***********:
 # result = np.zeros(img.shape, np.uint8)
 # for y in range(img.shape[0]):
 #     for x in range(img.shape[1]):
-#         ******* 3) 두 이미지의 좌측 상단 모서리가 닿도록 이미지 합치기 ***********:
+#         result[y][x] = img[y][x]
+#         if x < 100 and y < 100:
+#             result[y][x] = img_4color[y][x]
 
-#
-# cv.imshow("result", result)
+result = np.copy(img)
+for y in range(img_4color.shape[0]):
+    for x in range(img_4color.shape[1]):
+        result[y][x] = img_4color[y][x]
 
-
+cv.imshow("result", result)
 cv.waitKey()
